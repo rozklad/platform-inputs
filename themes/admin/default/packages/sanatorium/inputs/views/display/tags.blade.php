42baae85df71824de89102ee550e78cccf4d2312
rozklad <jan.rozklad@gmail.com>
2016-09-19 00:17:56 +0200

    automatic commit

--- themes/admin/default/packages/sanatorium/inputs/views/display/tags.blade.php
+++ themes/admin/default/packages/sanatorium/inputs/views/display/tags.blade.php
@@ -1,1 +1,8 @@
 {{-- Tags --}}
+@if ( is_array($entity->{$attribute->slug}) )
+    @if ( !empty($entity->{$attribute->slug}) )
+        @foreach( $entity->{$attribute->slug} as $tag )
+            <span class="label label-primary value-tag">{!! $tag !!}</span>
+        @endforeach
+    @endif
+@endif
