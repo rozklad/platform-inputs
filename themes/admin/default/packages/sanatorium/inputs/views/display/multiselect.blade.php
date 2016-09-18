aa2f2a404f9b27ae1ac8a8edfb7e9caed67f2ac0
rozklad <jan.rozklad@gmail.com>
2016-09-18 23:59:26 +0200

    automatic commit

--- themes/admin/default/packages/sanatorium/inputs/views/display/multiselect.blade.php
+++ themes/admin/default/packages/sanatorium/inputs/views/display/multiselect.blade.php
@@ -3,7 +3,9 @@
     @if ( !empty($entity->{$attribute->slug}) )
         @if ( is_array($entity->{$attribute->slug}) )
             @foreach( $entity->{$attribute->slug} as $option_key )
-                {!! textUrls2Links($attribute->options[$option_key]) !!}
+                @if ( isset($attribute->options[$option_key]) )
+                    {!! textUrls2Links($attribute->options[$option_key]) !!}
+                @endif
             @endforeach
         @endif
     @endif
--- themes/admin/default/packages/sanatorium/inputs/views/display/select.blade.php
+++ themes/admin/default/packages/sanatorium/inputs/views/display/select.blade.php
@@ -2,9 +2,13 @@
 @if ( is_array($entity->{$attribute->slug}) )
     @if ( !empty($entity->{$attribute->slug}) )
         @foreach( $entity->{$attribute->slug} as $option_key )
-            {!! textUrls2Links($attribute->options[$option_key]) !!}
+            @if ( isset($attribute->options[$option_key]) )
+                {!! textUrls2Links($attribute->options[$option_key]) !!}
+            @endif
         @endforeach
     @endif
 @else
-    {!! textUrls2Links($attribute->options[$entity->{$attribute->slug}]) !!}
+    @if ( isset($attribute->options[$entity->{$attribute->slug}]) )
+        {!! textUrls2Links($attribute->options[$entity->{$attribute->slug}]) !!}
+    @endif
 @endif
