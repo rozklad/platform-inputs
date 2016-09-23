e4327a8c9e6e0b79ea8e84fd5dd13e1741c725ca
rozklad <jan.rozklad@gmail.com>
2016-09-23 19:04:44 +0200

    automatic commit

---
+++ src/Validator/Attributes/AttributeValidator.php
@@ -0,0 +1,27 @@
+<?php namespace Sanatorium\Inputs\Validator\Attributes;
+
+use Platform\Attributes\Validator\AttributeValidatorInterface;
+use Cartalyst\Support\Validator;
+
+class AttributeValidator extends Validator implements AttributeValidatorInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected $rules = [
+        'name'    => 'required',
+        'slug'    => 'required|alpha_dash|unique:attributes',
+        'type'    => 'required',
+        'options' => 'required_if:type,checkbox,radio,select,multiselect',
+        'enabled' => 'required',
+    ];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function onUpdate()
+    {
+        //$this->rules['slug'] .= ',slug,{slug},slug';
+        $this->rules['slug'] = 'required|alpha_dash';
+    }
+}
