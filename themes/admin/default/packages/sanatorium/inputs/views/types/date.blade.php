1c7793e80b7ccaf85c25964cce83967726671a39
rozklad <jan.rozklad@gmail.com>
2016-09-26 02:10:10 +0200

    automatic commit

--- themes/admin/default/packages/sanatorium/inputs/views/types/date.blade.php
+++ themes/admin/default/packages/sanatorium/inputs/views/types/date.blade.php
@@ -1,10 +1,27 @@
+{{-- Queue assets --}}
+{{ Asset::queue('bootstrap-datepicker', 'sanatorium/inputs::bootstrap/bootstrap-datepicker.css') }}
+{{ Asset::queue('bootstrap-datepicker', 'sanatorium/inputs::bootstrap/bootstrap-datepicker.js', ['jquery']) }}
+
+{{-- Inline scripts --}}
+@section('scripts')
+    @parent
+    <script type="text/javascript">
+        $(function(){
+            $('#{{ $attribute->slug }}').datepicker({
+                format: 'yyyy-mm-dd',
+                weekStart: {{ (int)config('date.week.dow') }}
+            });
+        });
+    </script>
+@stop
+
 <div class="form-group{{ Alert::onForm($attribute->slug, ' has-error') }}">
 
     <label for="{{ $attribute->slug }}" class="control-label">
         {{{ transattr($attribute->slug, $attribute->name) }}}
     </label>
 
-    <input type="date" class="form-control" name="{{ $attribute->slug }}" id="{{ $attribute->slug }}" value="{{ $entity->{$attribute->slug} }}">
+    <input class="datepicker form-control" name="{{ $attribute->slug }}" id="{{ $attribute->slug }}" value="{{ $entity->{$attribute->slug} }}">
 
     <span class="help-block">{{{ Alert::onForm($attribute->slug) }}}</span>
 
