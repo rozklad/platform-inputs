f2b2d72c50a87f372ed1f225e9c196a3ad3f3a16
rozklad <jan.rozklad@gmail.com>
2016-10-18 16:23:23 +0200

    automatic commit

--- themes/frontend/default/packages/sanatorium/inputs/views/types/media.blade.php
+++ themes/frontend/default/packages/sanatorium/inputs/views/types/media.blade.php
@@ -10,10 +10,51 @@
 
     </label>
 
-    <input type="file" name="{{ $attribute->slug }}">
 
-    <div id="preview-container-{{ $attribute->slug }}">
-        @display($entity, $attribute->slug)
+    <div class="hidden">
+        <input type="file" name="{{ $attribute->slug }}" id="input-{{ $attribute->slug }}">
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6">
+            <div id="preview-container-{{ $attribute->slug }}">
+                @display($entity, $attribute->slug)
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <button type="button" class="btn btn-default" for="input-{{ $attribute->slug }}">
+                @if ( $entity->{$attribute->slug} )
+                    {{ 'Update picture' }}
+                @else
+                    {{ 'Add picture' }}
+                @endif
+            </button>
+        </div>
     </div>
 
 </div>
+
+@section('scripts')
+    @parent
+<script type="text/javascript">
+    $(function(){
+        $('[for="input-{{ $attribute->slug }}"]').click(function(event){
+            event.preventDefault();
+
+            var target = $(this).attr('for');
+
+            $('#' + target).trigger('click');
+        });
+    });
+</script>
+@stop
+
+@section('styles')
+    @parent
+    <style type="text/css">
+        .media-image-preview-{{ $attribute->slug }} img {
+            width: 100%;
+            height: auto;
+        }
+    </style>
+@stop
