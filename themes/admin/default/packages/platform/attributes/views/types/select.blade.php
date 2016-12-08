719a8f90e66096b9bdd4f50a6dfedef139e01057
rozklad <jan.rozklad@gmail.com>
2016-12-08 04:51:51 +0100

    automatic commit

--- themes/admin/default/packages/platform/attributes/views/types/select.blade.php
+++ themes/admin/default/packages/platform/attributes/views/types/select.blade.php
@@ -12,7 +12,14 @@
 
 	<select name="{{ $attribute->slug }}" id="{{ $attribute->slug }}" class="form-control">
 		@foreach ($attribute->options as $key => $value)
-		<option value="{{ $key }}" {{ request()->old($attribute->slug, $entity->exists ? $entity->{$attribute->slug} : null) == $key ? ' selected="selected"' : null }}>{{{ transattr($attribute->slug, $value, null, 'options', $key) }}}</option>
+		<option value="{{ $key }}" {{ request()->old($attribute->slug, $entity->exists ? $entity->{$attribute->slug} : null) == $key ? ' selected="selected"' : null }}>
+			<?php $label = transattr($attribute->slug, $value, null, 'options', $key); ?>
+			@if ( $label )
+				{{{ $label }}}
+			@else
+				{{{ $value }}}
+			@endif
+		</option>
 		@endforeach
 	</select>
 
